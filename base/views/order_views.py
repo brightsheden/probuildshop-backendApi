3d6ff7d745d172ea942ce8675d933e64c96b10c4
shedenbright <shedenbright@gmail.com>
2021-07-17 02:41:30 -0700

    fourth commit

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -1,72 +1,91 @@
+from django.shortcuts import render
 
-
-from rest_framework.decorators import api_view,permission_classes
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-#from .products import products
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from base.models import Product, Order,OrderItem,ShippingAddress
-from base.serializer import *
-from rest_framework import  serializers, status
-# Create your views here.
+
+from base.models import Product, Order, OrderItem, ShippingAddress
+from base.serializer import ProductSerializer, OrderSerializer
+
+from rest_framework import status
+from datetime import datetime
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def addOrderItems (request):
+def addOrderItems(request):
     user = request.user
     data = request.data
 
     orderItems = data['orderItems']
 
     if orderItems and len(orderItems) == 0:
-        message = {'deatail': 'No order item'}
-        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+        return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        # (1) create order
+
+        # (1) Create order
+
         order = Order.objects.create(
-            user = user,
-            paymentMethod = data['paymentMethod'],
-            taxPrice = data['taxPrice'],
-            shippingPrice= data['shippingPrice'],
-            totalPrice = data['totalPrice']
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice']
         )
-        # 2 create shipping address
+
+        # (2) Create shipping address
+
         shipping = ShippingAddress.objects.create(
-            order = order,
-            address = data['shippingAddress']['address'],
-            city = data['shippingAddress']['city'],
-            postalCode = data['shippingAddress']['postalCode'],
-            country = data['shippingAddress']['country'],
-
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
         )
-        # 3  create orderItems and set order to orderItem relationship
 
+        # (3) Create order items adn set order to orderItem relationship
         for i in orderItems:
             product = Product.objects.get(_id=i['product'])
 
-            #product = Product.objects.get(_id=1['product'])
-
             item = OrderItem.objects.create(
-                product = product,
-                order = order,
-                name = product.name,
-                qty = i['qty'],
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
                 price=i['price'],
-                image = product.image.url
+                image=product.image.url,
             )
 
-        # 4 update stock
-            product.countInstock -= item.qty
+            # (4) Update stock
+
+            product.countInStock -= item.qty
             product.save()
 
         serializer = OrderSerializer(order, many=False)
-
         return Response(serializer.data)
 
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getMyOrders(request):
+    user = request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getOrderById(request, pk):
+
     user = request.user
 
     try:
@@ -75,8 +94,24 @@ def getOrderById(request, pk):
             serializer = OrderSerializer(order, many=False)
             return Response(serializer.data)
         else:
-            Response({"detail":"Not authorised to view this order"},
-            status=status.HTTP_400_BAD_REQUEST)
+            Response({'detail': 'Not authorized to view this order'},
+                     status=status.HTTP_400_BAD_REQUEST)
     except:
-        return Response({"detail":"order doese not exist"}, status=status.HTTP_400_BAD_REQUEST)
-        
+        return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+
+    return Response('Order was paid')
+
+
+
+
+
--- base/views/user_views.py
+++ base/views/user_views.py
@@ -1,33 +1,31 @@
-from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
-from rest_framework.decorators import api_view,permission_classes
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-#from .products import products
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from base.serializer import *
+from django.contrib.auth.models import User
+from base.serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import  status
-from django.contrib.auth.models import User
 
-#token_views
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
+
         serializer = UserSerializerWithToken(self.user).data
-        for k,v in serializer.items():
-            data[k] = v 
-       
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
 
 
 @api_view(['POST'])
@@ -50,7 +48,7 @@ def registerUser(request):
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
-def  updateUserProfile(request):
+def updateUserProfile(request):
     user = request.user
     serializer = UserSerializerWithToken(user, many=False)
 
@@ -59,27 +57,64 @@ def  updateUserProfile(request):
     user.username = data['email']
     user.email = data['email']
 
-    if user.password != "":
-        user.password =make_password(data['password'])
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
     user.save()
 
+    return Response(serializer.data)
 
-    return  Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def  getUserProfile(request):
+def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
-    return  Response(serializer.data)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
-def  getUsers(request):
-    user= User.objects.all()
-    serializer = UserSerializer(user, many=True)
-    return  Response(serializer.data)
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUserById(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+
+    data = request.data
+
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+
+    user.save()
+
+    serializer = UserSerializer(user, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request, pk):
+    userForDeletion = User.objects.get(id=pk)
+    userForDeletion.delete()
+    return Response('User was deleted')
+
 
 
 
